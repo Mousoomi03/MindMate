ff4ec8dbd1f899195c793a1328b8497e6966295c
Mousoomi <141304500+Mousoomi03@users.noreply.github.com>
2025-12-23 21:35:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ genai.configure(api_key=st.secrets["GEMINI_KEY"])
 
 # LOGIC FUNCTIONS
 
-dfrom youtube_transcript_api import YouTubeTranscriptApi
-
 def get_video_transcript(video_url):
     '''Extracts transcript from YouTube video ID for free'''
     try:
